3ae69d8119637bfb5018d21ea43a12293bcb9862
gulsah <ulger.gulsah@gmail.com>
2026-09-16 00:00:41 +0200

    Add Spring Boot project skeleton

---
+++ src/main/java/com/gulsah/mini_doodle/MiniDoodleApplication.java
@@ -0,0 +1,13 @@
+package com.gulsah.mini_doodle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniDoodleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniDoodleApplication.class, args);
+	}
+
+}
